e1fb7fbda1f5e5513e4686774a6a036b47955cb6
exiang01 <exiang01@students.cs.ubc.ca>
2024-11-28 05:22:55 -0800

    script change and frontend insert

--- public/js/builder.js
+++ public/js/builder.js
@@ -1,7 +1,8 @@
 window.onload = function() {
     loadPcPartsListIDs();
     document.getElementById("pcPartsListIDForm").addEventListener("submit", loadPcPartsList);
-    document.getElementById("deletePartID").addEventListener("submit", deletePcPartFromList);
+    document.getElementById("pcPartsListInsertForm").addEventListener("submit", insertPcPartFromList);
+    document.getElementById("pcPartsListDeleteform").addEventListener("submit", deletePcPartFromList);
 };
 
 let currentListID;
@@ -72,6 +73,32 @@ async function loadPcPartsList(event) {
     }
 }
 
+// Given a partID insert the part to the list currently selected
+
+async function insertPcPartFromList(event) {
+    event.preventDefault();
+    const tableElement = document.getElementById('listOfListIDs');
+    const tableBody = tableElement.querySelector('tbody');
+
+    const response = await fetch('/TODO', {
+        method: 'GET'
+    });
+
+    const responseData = await response.json();
+    const tableContent = responseData.data;
+
+    if (tableBody) {
+        tableBody.innerHTML = '';
+    }
+
+    tableContent.forEach(part => {
+        const row = tableBody.insertRow();
+        part.forEach((field, index) => {
+            const cell = row.insertCell(index);
+            cell.textContent = field;
+        });
+    });
+}
 
 
 // Given a partID delete the part from the list currently selected
