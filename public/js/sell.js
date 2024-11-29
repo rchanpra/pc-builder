72acaab536c36e260aa22ae1c307efa75f602d59
exiang01 <exiang01@students.cs.ubc.ca>
2024-11-29 02:37:58 -0800

    changes to sell/market for filter, and css changes

--- public/js/sell.js
+++ public/js/sell.js
@@ -1,6 +1,69 @@
 window.onload = function() {
     loadSell();
+    document.getElementById("addFilter").addEventListener("click", addNewFilterForm);
+    document.getElementById("submitFilterSearch").addEventListener("submit", runFilter);
 };
+
+async function addNewFilterForm(event) {
+    event.preventDefault();
+
+    let mainFilterDiv = document.getElementById("filterSection")
+    let newFilterDiv = mainFilterDiv.children[0].cloneNode(true);
+    mainFilterDiv.appendChild(newFilterDiv);
+    newFilterDiv.querySelector('input[name="searchBar"]').value = '';
+    newFilterDiv.querySelectorAll('select').forEach((select) => {
+        select.selectedIndex = 0;
+    });
+    newFilterDiv.querySelector('button[name="removeFilter"]').addEventListener("click", (event) => {
+        event.preventDefault();
+        if(mainFilterDiv.children.length > 1) {
+            newFilterDiv.remove();
+        }
+    });
+}
+
+async function runFilter(event) {
+    event.preventDefault();
+
+    const tableElement = document.getElementById('SellTable');
+    const tableBody = tableElement.querySelector('tbody');
+
+    let filterList = []
+    
+    document.getElementById("filterSection").querySelectorAll().forEach((child) => {
+
+    })
+
+
+    const response = await fetch('/update', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            PartID: idValue,
+            Name: nameValue,
+            Model: modelValue,
+            Rating: ratingValue,
+            ManufacturerID: manufacturerValue
+        })
+    });
+
+    const responseData = await response.json();
+    const tableContent = responseData.data;
+
+    if (tableBody) {
+        tableBody.innerHTML = '';
+    }
+
+    tableContent.forEach(part => {
+        const row = tableBody.insertRow();
+        part.forEach((field, index) => {
+            const cell = row.insertCell(index);
+            cell.textContent = field;
+        });
+    });
+}
 
 async function loadSell() {
     const tableElement = document.getElementById('SellTable');
